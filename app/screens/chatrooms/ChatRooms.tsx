b6c4809c008d623d6c00dadbc6320812fc0d285c
김태욱 <104901712+Woo7i@users.noreply.github.com>
2024-05-12 03:22:50 +0900

    Revert "Feature/chatrooms"

--- app/screens/chatrooms/ChatRooms.tsx
+++
@@ -1,5 +0,0 @@
-import { Text } from "react-native";
-
-export function ChatRooms(): React.JSX.Element {
-  return <Text>ChatRooms</Text>;
-}
--- app/screens/home/Home.tsx
+++
@@ -1,5 +0,0 @@
-import { Text } from "react-native";
-
-export function Home(): React.JSX.Element {
-  return <Text>Home</Text>;
-}
